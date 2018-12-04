5c9eef402d5616dfcb87d3b368d8cda2b6cb3196
Damian Krzeminski <krzemdamian@gmail.com>
2018-12-04 21:23:00 +0100

    Add class Revit External Command to send object.
    This is class ivoked by command on ribbon.
    It is uesd to send object stored in Revit app to the worker waiting for message.
    Worker have to have pipe connected.

---
+++ App_Framework/Sender.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Framework
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.ReadOnly)]
+    class Sender : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog taskDialog = new TaskDialog("Object sender");
+            taskDialog.CommonButtons = TaskDialogCommonButtons.Yes | TaskDialogCommonButtons.No;
+            taskDialog.DefaultButton = TaskDialogResult.Yes;
+            taskDialog.MainInstruction = "Do yo want to send object?";
+            TaskDialogResult res = taskDialog.Show();
+            if (res == TaskDialogResult.Yes)
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    Serializer.Serialize(stream, ComunicationServer.TestObject);
+                    ComunicationServer.ServerPipe.WriteBytes(stream.ToArray());
+                }
+            }
+            
+            return Result.Succeeded;
+        }
+    }
+}
